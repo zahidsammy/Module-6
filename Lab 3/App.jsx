8aac6983c86e3319340f5dca0a87b369ad04fea0
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-11-21 15:29:44 +1300

    Add files via upload

---
+++ Lab 3/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Emoji from './Emoji'; // Import the Emoji component
+
+const App = () => {
+  return (
+    <div>
+      <h1>Emoji Changer</h1>
+      <Emoji />
+    </div>
+  );
+};
+
+export default App;
+
